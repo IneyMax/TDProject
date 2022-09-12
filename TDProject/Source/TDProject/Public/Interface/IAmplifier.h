3c2f13258de8907de11d1319de8ffa7922f67407
IneyMax <mr.algree@gmail.com>
2022-09-12 20:56:37 +0300

    Add IAmplifier.h, IItemInteract.h, IWeapon.h

---
+++ TDProject/Source/TDProject/Public/Interface/IAmplifier.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "IAmplifier.generated.h"
+
+
+UINTERFACE(Blueprintable, MinimalAPI)
+class UIAmplifier : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/* Amplifiers Interface */
+class TDPROJECT_API IIAmplifier
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category=IAmplifier)
+	void UseAmplifier();
+};
